644b04847b5dcae58260ee9c268211cb336e7b8a
Martin Dimitrievski <Martin@Martins-MacBook-Pro.local>
2025-03-13 12:02:32 +0100

    removing mailosaur utility, might add it later if i have time to verify user registration

--- pages/spaces/spaces.creation.page.ts
+++ pages/spaces/spaces.creation.page.ts
@@ -1,12 +1,10 @@
 import { Browser, Locator, Page, expect } from '@playwright/test';
 import { BasePage } from '../base/base.page';
-import { MailosaurUtility } from '../../lib/mailosaur';
 import { Helpers } from '../../lib/helpers.ts';
 import path from 'path';
 
 export class SpacesCreationPage extends BasePage {
     readonly page: Page;
-    private mailosaur: MailosaurUtility;
     private helpers: Helpers;
     readonly createSpaceBotton: Locator;
 
